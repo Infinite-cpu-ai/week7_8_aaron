9cdf5bf80eeffc46e1f1760906542a73486cc3d4
Infinite-cpu-ai <hanzelius01@student.ciputra.ac.id>
2025-10-23 08:01:35 +0700

    import image and some background background added

--- app/src/main/java/com/hanzelius/week7_8_aaron/ui/view/MainView.kt
+++ app/src/main/java/com/hanzelius/week7_8_aaron/ui/view/MainView.kt
@@ -1,2 +1,29 @@
 package com.hanzelius.week7_8_aaron.ui.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.hanzelius.week7_8_aaron.R
+
+@Composable
+fun MainView(modifier: Modifier = Modifier) {
+    Column (
+        modifier.fillMaxSize()
+            .paint(
+                painter = painterResource(id = R.drawable.ic_launcher_background)
+            )
+    ){
+
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MainViewPreview(){
+    MainView()
+}
