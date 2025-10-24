c6131d5d3a59ca27ec664bdb38e77838bf37fdae
Infinite-cpu-ai <hanzelius01@student.ciputra.ac.id>
2025-10-24 12:05:55 +0700

    fix fix fix container

--- app/src/main/java/com/hanzelius/week7_8_aaron/data/container/WeatherContainer.kt
+++ app/src/main/java/com/hanzelius/week7_8_aaron/data/container/WeatherContainer.kt
@@ -1,9 +1,11 @@
 package com.hanzelius.week7_8_aaron.data.container
 
 import com.google.gson.GsonBuilder
+import com.hanzelius.week7_8_aaron.data.repository.WeatherRepository
 import com.hanzelius.week7_8_aaron.data.service.WeatherService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.getValue
 
 class WeatherContainer {
 
@@ -18,5 +20,9 @@ class WeatherContainer {
 
     private val retrofitService: WeatherService by lazy {
         retrofit.create(WeatherService::class.java)
+    }
+
+    private val weatherRepository: WeatherRepository by lazy {
+        WeatherRepository(retrofitService)
     }
 }
