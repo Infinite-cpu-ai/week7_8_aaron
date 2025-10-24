379fb165bff09f3459ef49705c4c7c8426aedff7
Infinite-cpu-ai <hanzelius01@student.ciputra.ac.id>
2025-10-24 08:35:30 +0700

    weather array

--- app/src/main/java/com/hanzelius/week7_8_aaron/ui/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/hanzelius/week7_8_aaron/ui/viewmodel/WeatherViewModel.kt
@@ -1,32 +1,30 @@
 package com.hanzelius.week7_8_aaron.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import com.hanzelius.week7_8_aaron.R
+import com.hanzelius.week7_8_aaron.data.container.WeatherContainer
+import com.hanzelius.week7_8_aaron.data.dto.WeatherItem
+import com.hanzelius.week7_8_aaron.data.repository.WeatherRepository
+import com.hanzelius.week7_8_aaron.ui.model.WeatherResponse
 import kotlinx.coroutines.launch
+import okhttp3.Response
+
 
 class WeatherViewModel(private val repository: WeatherRepository) : ViewModel() {
 
-    private val _state = MutableStateFlow<WeatherState>(WeatherState.Empty)
-    val state: StateFlow<WeatherState> = _state
+    private val _weather = MutableLiveData<WeatherResponse>()
+    val weather: LiveData<WeatherResponse> = _weather
 
-    fun fetchWeather(city: String, apiKey: String) {
-        viewModelScope.launch {
-            try {
-                _state.value = WeatherState.Loading
-                val response = repository.getWeather(city, apiKey)
-                _state.value = WeatherState.Success(response)
-            } catch (e: Exception) {
-                _state.value = WeatherState.Error("HTTP 404 Not Found")
-            }
-        }
-    }
-}
+    val listDetail = arrayListOf(
+        arrayListOf(R.drawable.icon_humidity, "HUMIDITY", "${weather.value.humidityPercentage}%"),
+        arrayListOf(R.drawable.icon_wind, "WIND SPEED", "${weather.value.windSpeed} m/s"),
+        arrayListOf(R.drawable.icon_feels_like, "FEELS LIKE", "${weather.value.tempFeelsLike}°C"),
+        arrayListOf(R.drawable.vector_2, "RAIN FALL", "${weather.value.rainFall} mm"),
+        arrayListOf(R.drawable.devices, "PRESSURE", "${weather.value.pressure}hPa"),
+        arrayListOf(R.drawable.cloud, "CLOUDINESS", "${weather.value.cloudiness}%")
+    )
 
-sealed class WeatherState {
-    object Empty : WeatherState()
-    object Loading : WeatherState()
-    data class Success(val data: WeatherResponse) : WeatherState()
-    data class Error(val message: String) : WeatherState()
 }
