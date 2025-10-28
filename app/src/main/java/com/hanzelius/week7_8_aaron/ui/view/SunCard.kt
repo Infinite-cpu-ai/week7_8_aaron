a1a00557827d6c5033222bded2d28fac7e031548
Infinite-cpu-ai <hanzelius01@student.ciputra.ac.id>
2025-10-28 08:25:00 +0700

    suncard done all

--- app/src/main/java/com/hanzelius/week7_8_aaron/ui/view/SunCard.kt
+++ app/src/main/java/com/hanzelius/week7_8_aaron/ui/view/SunCard.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
