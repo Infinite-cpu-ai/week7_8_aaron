809ec42e5b4ecc10b06dc9063a352a84ef5c67f2
Infinite-cpu-ai <hanzelius01@student.ciputra.ac.id>
2025-10-28 08:23:29 +0700

    detailcard done

--- app/src/main/java/com/hanzelius/week7_8_aaron/ui/view/DetailCard.kt
+++ app/src/main/java/com/hanzelius/week7_8_aaron/ui/view/DetailCard.kt
@@ -1,7 +1,6 @@
 package com.hanzelius.week7_8_aaron.ui.view
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,11 +8,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
